b9b8d64e1be130887395efc50b8371adfdaace3b
MYNAMEVLADIK <zitarvlad28@mail.ru>
2023-08-07 15:12:27 +0700

    5 commit

--- src/main/java/ru/skypro/homework/repository/UserRepository.java
+++ src/main/java/ru/skypro/homework/repository/UserRepository.java
@@ -1,4 +1,16 @@
 package ru.skypro.homework.repository;
 
-public interface UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import ru.skypro.homework.entity.User;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    /**
+     * Поиск пользователя по email
+     */
+    Optional<User> findByEmail(String email);
 }
